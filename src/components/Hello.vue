b1a084640e8cff9da129764b5fcb3823673d848b
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-11-22 00:53:45 +0700

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <section class="hero has-text-centered is-bold is-info">
+      <div class="hero-head">
+        <div class="container">
+          <nav class="nav">
+            <div class="nav-left">
+              <a class="nav-item is-brand" href="#"> 
+                <h1 class="title">THYPE!!</h1>
+              </a>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ msg }}</h1>
+        </div>
+      </div>
+    </section>
+    <br>
+    <section>
+      <div class="container">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent is-vertical is-2">
+            <article class="tile is-child notification is-danger">
+              <p class="title">Stat</p>
+              <p class="subtitle">Player statistics</p>
+            </article>
+            <nav class="panel tile is-child is-warning">
+              <p class="panel-heading">History</p>
+              <a class="panel-block">Match 01</a>
+              <a class="panel-block">Match 02</a>
+              <a class="panel-block">Match 03</a>
+              <a class="panel-block">Match 04</a>
+            </nav>
+          </div>
+          <div class="tile is-parent is-8 is-vertical">
+            <div class="box tile is-child">
+              <p class="title">Play area</p>
+              <div class="typee-content">
+                <div id="app">
+                  {{ message }}
+                </div>
+              </div>
+              <div id="app-2">
+                <span v-bind:title="message">
+                  Hover your mouse over me for a few seconds to see my dynamically bound title!
+                </span>
+              </div>
+              <div id="app-3">
+                <p v-if="seen">Now you see me</p>
+              </div>
+              <div id="app-7">
+                <ol>
+                  
+                </ol>
+              </div>
+              <div id="app-8">
+                
+              </div>
+              <div id="app-9">
+                <button class="button is-primary" v-on:click="increment">Incr</button>
+              </div>          
+            </div>
+            <article class="tile is-child notification is-light">
+              <p class="title">Typee Info</p>
+              <p>Lorem Ipsum            </p>
+            </article>
+          </div>
+          <div class="tile is-parent is-vertical is-2">
+            <article class="tile is-child notification is-info">
+              <p class="title">User profile</p>
+              <p class="subtitle">Guest</p>
+            </article>
+            <article class="tile is-child notification is-success">
+              <p class="title">Friend list</p>
+              <p class="subtitle">User's friend list</p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome ^_^ Let\'s thype'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
