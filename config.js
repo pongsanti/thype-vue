b9cb5553712b191b5d4ad595acc03301e13129c5
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-11-27 16:51:20 +0700

    config for heroku

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 module.exports = () => {
   switch (process.env.NODE_ENV) {
     case 'production':
-      return {}
+      return {
+        PORT: process.env.PORT || 5000
+      }
     default:
       return {
         MONGODB_URI: 'mongodb://localhost:27017/thype',
